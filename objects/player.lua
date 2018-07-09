7fec274372bf32f8cc5bc4039fe23e91675bf2a9
Odd Chromosome <39212643+oddchromosome@users.noreply.github.com>
2018-07-09 09:57:48 +0300

    bloody collisions

--- objects/player.lua
+++ objects/player.lua
@@ -27,33 +27,12 @@ function Player:new(x,y)
 
     camera:goto_point(self.pos)
 
-    self.vel.y = self.vel.y + self.gravity * dt
-    self.dir.y = 1
-
-    -- collisions
-    local x, y = game:get_cell(self.pos.x), game:get_cell(self.pos.y)
-
-    local d = 1
-
-    for i = y,y+d do
-      for j = x,x+d do
-        local tile = tileMap:get(tile_layer,i,j)
-        if tile == nil then goto cont end
-
-        local box = rect:new(self.pos.x,self.pos.y,self.size.x, self.size.y)
-
-        local coll,rect = rectangle_collision(box, tile)
-        if coll then
-          self.vel.y = 0
-          self.dir.y = 0
-
-          if self.pos.y + self.size.y > rect.pos.y then
-            self.pos.y = rect.pos.y - self.size.y
-          end
-        end
-
-        ::cont::
-      end
+    if not self.on_ground then
+      self.dir.y = 1
+      self.vel.y = self.vel.y + self.gravity * dt
+    else
+      self.dir.y = 0
+      self.vel.y = 0
     end
 
     if key("left") then
@@ -66,14 +45,49 @@ function Player:new(x,y)
       self.vel.x = 50
     end
 
+    local delta_x,delta_y = 0,0
+
+    -- x velocity
+    delta_x = self.vel.x * dt * self.dir.x
+    self.pos.x = self.pos.x + delta_x
+
+    if not self.on_ground then
+      delta_y = self.vel.y * dt * self.dir.y
+      self.pos.y = self.pos.y + delta_y
+    end
+
+    -- jump
     if key("up") then
+      self.on_ground = false
       if self.vel.y < 10 and self.vel.y > -10 then
-        self.vel.y = -self.gravity * .5
+        self.dir.y = -1
+        self.vel.y = self.dir.y * self.gravity * .5
       end
     end
 
-    self.pos.x = self.pos.x + (self.vel.x * dt) * self.dir.x
-    self.pos.y = self.pos.y + (self.vel.y * dt) * self.dir.y
+    -- collisions
+    local suspect_coordinates = collision_suspect(self)
+    if suspect_coordinates then
+      for i = 1,#suspect_coordinates do
+        local point = suspect_coordinates[i]
+        local tile = tileMap:get(tile_layer,point[2],point[1])
+        if tile ~= nil then
+          local collisions = rectangle_collision(self, tile, delta_x, delta_y)
+          for c = 1,#collisions do
+            if collisions[c] == "up" or collisions[c] == "down" then
+              self.vel.y = 0
+              self.dir.y = 0
+              if collisions[c] == "down" then
+                self.on_ground = true
+              end
+            elseif collisions[c] == "right" or collisions[c] == "left" then
+              self.vel.x = 0
+              self.dir.x = 0
+            end
+          end
+        end
+      end
+    end
 
     self.vel.x = self.vel.x * (1 - dt*12)
   end
--- tools/physics.lua
+++ tools/physics.lua
@@ -1,13 +1,119 @@
-function rectangle_collision(rect_1, rect_2)
+game              = require "game"
 
+function collision_suspect(moving)
+  local suspects = {}
+
+  local x, y = game:get_cell(moving.pos.x), game:get_cell(moving.pos.y)
+
+  if moving.dir.x > 0 then
+    table.insert(suspects, {x+1,  y-1})
+    table.insert(suspects, {x+1,  y})
+    table.insert(suspects, {x+1,  y+1})
+  elseif moving.dir.x < 0 then
+    table.insert(suspects, {x-1,  y-1})
+    table.insert(suspects, {x-1,  y})
+    table.insert(suspects, {x-1,  y+1})
+  end
+
+  if moving.dir.y > 0 then
+    table.insert(suspects, {x-1,y+1})
+    table.insert(suspects, {x,  y+1})
+    table.insert(suspects, {x+1,y+1})
+  elseif moving.dir.y < 0 then
+    table.insert(suspects, {x-1,y-1})
+    table.insert(suspects, {x,  y-1})
+    table.insert(suspects, {x+1,y-1})
+  end
+
+  return suspects
+end
+
+function rectangle_collision(rect_1, rect_2)
+  collisions = {}
+  if collision_right(rect_1,rect_2) then
+    table.insert(collisions, "right")
+  end
+  if collision_left(rect_1,rect_2) then
+    table.insert(collisions, "left")
+  end
+  if collision_up(rect_1,rect_2) then
+    table.insert(collisions, "up")
+  end
+  if collision_down(rect_1,rect_2) then
+    table.insert(collisions, "down")
+  end
+  
+  return collisions
+end
+
+function collision_right(rect_1,rect_2)
+  -- right
   if rect_1.pos.x + rect_1.size.x > rect_2.pos.x and
-     rect_1.pos.x < rect_2.pos.x + rect_2.size.x and
+     rect_1.pos.x + rect_1.size.x < rect_2.pos.x + rect_2.size.x/2 and
      rect_1.pos.y + rect_1.size.y > rect_2.pos.y and
      rect_1.pos.y < rect_2.pos.y + rect_2.size.y then
 
-    return true,rect_2
-
+    snap_right(rect_1,rect_2)
+    return true
   end
 
-  return false,nil
+  return false
+end
+
+function collision_left(rect_1,rect_2)
+  -- left
+  if rect_1.pos.x < rect_2.pos.x + rect_2.size.x and
+     rect_1.pos.x > rect_2.pos.x + rect_2.size.x/2 and
+     rect_1.pos.y + rect_1.size.y > rect_2.pos.y and
+     rect_1.pos.y < rect_2.pos.y + rect_2.size.y then
+
+    snap_left(rect_1,rect_2)
+    return true
+  end
+
+  return false
+end
+
+function collision_up(rect_1,rect_2)
+  -- up
+  if rect_1.pos.y + rect_1.size.y > rect_2.pos.y and
+     rect_1.pos.y + rect_1.size.y < rect_2.pos.y + rect_2.size.y/2 and
+     rect_1.pos.x + rect_1.size.x > rect_2.pos.x and
+     rect_1.pos.x < rect_2.pos.x + rect_2.size.x then
+
+    snap_up(rect_1,rect_2)
+    return true
+  end
+
+  return false
+end
+
+function collision_down(rect_1,rect_2)
+  -- down
+  if rect_1.pos.y < rect_2.pos.y + rect_2.size.y and
+     rect_1.pos.y > rect_2.pos.y + rect_2.size.y/2 and
+     rect_1.pos.x + rect_1.size.x > rect_2.pos.x and
+     rect_1.pos.x < rect_2.pos.x + rect_2.size.x then
+
+    snap_down(rect_1,rect_2)
+    return true
+  end
+
+  return false
+end
+
+function snap_up(moving,static)
+  moving.pos.y = static.pos.y - moving.size.y
+end
+
+function snap_down(moving,static)
+  moving.pos.y = static.pos.y + static.size.y
+end
+
+function snap_right(moving,static)
+  moving.pos.x = static.pos.x - moving.size.x
+end
+
+function snap_left(moving,static)
+  moving.pos.x = static.pos.x + static.size.x
 end
