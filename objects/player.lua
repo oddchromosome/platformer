38cf73cbd6439cf0678abe2b7a59ca21c9acf5ad
Odd Chromosome <39212643+oddchromosome@users.noreply.github.com>
2018-07-09 10:36:01 +0300

    back to old collisions

--- objects/player.lua
+++ objects/player.lua
@@ -27,13 +27,8 @@ function Player:new(x,y)
 
     camera:goto_point(self.pos)
 
-    if not self.on_ground then
-      self.dir.y = 1
-      self.vel.y = self.vel.y + self.gravity * dt
-    else
-      self.dir.y = 0
-      self.vel.y = 0
-    end
+    self.vel.y = self.vel.y + self.gravity * dt
+    self.dir.y = 1
 
     if key("left") then
       self.dir.x = -1
@@ -45,49 +40,73 @@ function Player:new(x,y)
       self.vel.x = 50
     end
 
-    local delta_x,delta_y = 0,0
-
-    -- x velocity
-    delta_x = self.vel.x * dt * self.dir.x
-    self.pos.x = self.pos.x + delta_x
-
-    if not self.on_ground then
-      delta_y = self.vel.y * dt * self.dir.y
-      self.pos.y = self.pos.y + delta_y
-    end
-
-    -- jump
-    if key("up") then
-      self.on_ground = false
-      if self.vel.y < 10 and self.vel.y > -10 then
-        self.dir.y = -1
-        self.vel.y = self.dir.y * self.gravity * .5
-      end
-    end
-
     -- collisions
-    local suspect_coordinates = collision_suspect(self)
-    if suspect_coordinates then
-      for i = 1,#suspect_coordinates do
-        local point = suspect_coordinates[i]
-        local tile = tileMap:get(tile_layer,point[2],point[1])
-        if tile ~= nil then
-          local collisions = rectangle_collision(self, tile, delta_x, delta_y)
-          for c = 1,#collisions do
-            if collisions[c] == "up" or collisions[c] == "down" then
-              self.vel.y = 0
-              self.dir.y = 0
-              if collisions[c] == "down" then
-                self.on_ground = true
-              end
-            elseif collisions[c] == "right" or collisions[c] == "left" then
+    local x, y = game:get_cell(self.pos.x), game:get_cell(self.pos.y)
+    local d = 1
+
+    for i = y-d,y+d do
+      for j = x-d,x+d do
+        local tile = tileMap:get(tile_layer,i,j)
+        if tile == nil then goto cont end
+
+        local box = rect:new(self.pos.x + self.vel.x * dt * self.dir.x,self.pos.y + self.vel.y * dt * self.dir.y,self.size.x,self.size.y)
+
+        local coll,rect = rectangle_collision(box, tile)
+        if coll then
+          self.vel.y = 0
+          self.dir.y = 0
+
+          -- y collisions
+          if self.pos.y + self.size.y / 2 < rect.pos.y + rect.size.y / 2 then
+
+            -- fix bounds
+            if box.pos.y + box.size.y > rect.pos.y and
+               self.pos.y + self.size.y < rect.pos.y + game.sprite / 2 then
+
+              self.pos.y = rect.pos.y - self.size.y
+            end
+          else
+            self.vel.y = -self.vel.y
+            -- goto cont
+          end
+
+          -- x collisions
+          if self.pos.x < rect.pos.x + rect.size.x/2 then
+
+            --fix bounds left
+            if box.pos.x + box.size.x > rect.pos.x and
+               self.pos.y + self.size.y > rect.pos.y then
+
               self.vel.x = 0
               self.dir.x = 0
+
+              self.pos.x = rect.pos.x - rect.size.x
+            end
+          else
+            --fix bounds right
+            if box.pos.x < rect.pos.x + rect.size.x and
+               self.pos.y + self.size.y > rect.pos.y then
+
+              self.vel.x = 0
+              self.dir.x = 0
+
+              self.pos.x = rect.pos.x + rect.size.x
             end
           end
         end
+
+        ::cont::
       end
     end
+
+    if key("up") then
+      if self.vel.y < 10 and self.vel.y > -10 then
+        self.vel.y = -self.gravity * .5
+      end
+    end
+
+    self.pos.x = self.pos.x + (self.vel.x * dt) * self.dir.x
+    self.pos.y = self.pos.y + (self.vel.y * dt) * self.dir.y
 
     self.vel.x = self.vel.x * (1 - dt*12)
   end
